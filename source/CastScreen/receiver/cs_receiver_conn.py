ad7f2ebef7974f06bd8e8d4764317d4ea6b91fd1
fdarbeha <fatemeh.darbehani@gmail.com>
2017-07-05 16:15:30 -0700

    Update receiver to work with python3

--- source/CastScreen/receiver/cs_receiver_conn.py
+++ source/CastScreen/receiver/cs_receiver_conn.py
@@ -37,7 +37,7 @@ SAVE_TO_FILE = True
 def connect_to_server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = ('localhost', PORT)
-    print 'Connecting to %s port %s' % server_address
+    print ('Connecting to %s port %s' % server_address)
     sock.connect(server_address)
 
     cv2.startWindowThread()
@@ -45,10 +45,10 @@ def connect_to_server():
     try:
         # Send data
         message = 'mirror\n'
-        print 'Sending mirror cmd'
+        print ('Sending mirror cmd')
         sock.sendall(message)
 
-	if SAVE_TO_FILE:
+        if SAVE_TO_FILE:
             f = open('video_client.raw', 'wb')
         p = Popen(['ffplay', '-framerate', '30', '-'], stdin=PIPE, stdout=PIPE)
         #p = Popen(['gst-launch-1.0', 'fdsrc', '!', 'h264parse', '!', 'avdec_h264', '!', 'autovideosink'], stdin=PIPE, stdout=PIPE)
@@ -56,17 +56,17 @@ def connect_to_server():
         while True:
             data = sock.recv(bufferSize)
             if data == None or len(data) <= 0:
-                print 'No data\n'
+                print ('No data\n')
                 break
             #print 'receiving data\n'
             if not skiped_metadata:
                 if data.find('\r\n\r\n') > 0:
                     last_ctrl = data.find('\r\n\r\n') + 4
-                    print 'Recv control data: ', data[0:last_ctrl]
-                    print 'Size of data: ', getsizeof(data)
+                    print ('Recv control data: ', data[0:last_ctrl])
+                    print ('Size of data: ', getsizeof(data))
                     if len(data) > last_ctrl:
                         p.stdin.write(data[last_ctrl:])
-	                if SAVE_TO_FILE:
+                        if SAVE_TO_FILE:
                             f.write(data[last_ctrl:])
                 skiped_metadata = True
             else:
@@ -77,10 +77,10 @@ def connect_to_server():
                 #print frame
                 #cv2.imshow('frame',frame)
                 #cv2.waitKey(0)
-	        if SAVE_TO_FILE:
+                if SAVE_TO_FILE:
                     f.write(data)
         p.kill()
-	if SAVE_TO_FILE:
+        if SAVE_TO_FILE:
             f.close()
 
     finally:
